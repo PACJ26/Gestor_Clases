4331cfd36f811cc849be3dfcf00e2df44c0aaeb5
PACJ26 <juan123alv@gmail.com>
2025-03-08 15:20:09 -0500

    se agrega la nueva consulta SQL, se agrega fucion para generar colores aleatorios, se agrega en arrya clases el estado junto con el titulo

--- Controladores/obtener_clases.php
+++ Controladores/obtener_clases.php
@@ -1,7 +1,15 @@
 <?php
+function generarColorDesdeHora($hora) {
+    $hash = md5($hora);
+    $r = hexdec(substr($hash, 0, 2)) % 156 + 100; 
+    $g = hexdec(substr($hash, 2, 2)) % 156 + 100;
+    $b = hexdec(substr($hash, 4, 2)) % 156 + 100;
+    return "rgb($r, $g, $b)";
+}
+
 include("../Conexion/conexion.php");
 
-$sql = "SELECT c.id, a.nombre as asignatura, c.fecha, c.hora, c.hora_fin, u.nombres as profesor 
+$sql = "SELECT c.id, a.nombre as asignatura, c.fecha, c.hora, c.hora_fin, c.estado, u.nombres as profesor 
         FROM clases c
         INNER JOIN asignaturas a ON c.asignatura_id = a.id 
         INNER JOIN usuarios u ON c.profesor_id = u.id";
@@ -10,16 +18,24 @@ $resultado = $conexion->query($sql);
 
 $clases = [];
 while ($fila = $resultado->fetch_assoc()) {
+    $horaInicio = date("H:i:s", strtotime($fila["hora"]));
+    $horaFin = date("H:i:s", strtotime($fila["hora_fin"]));
+    $fecha = date("Y-m-d", strtotime($fila["fecha"]));
+
+    $colorFondo = generarColorDesdeHora($horaInicio);
+
     $clases[] = [
-        "id" =>  $fila["id"],
-        "title" => $fila["asignatura"] . " - " . $fila["profesor"],
-        "start" => $fila["fecha"] . "T" . $fila["hora"],
-        "end" => $fila["fecha"] . "T" . $fila["hora_fin"], 
+        "id" => $fila["id"],
+        "title" => $fila["asignatura"] . " - " . $fila["profesor"] . " (" . $fila["estado"] . ")",
+        "start" => "{$fecha}T{$horaInicio}",
+        "end" => "{$fecha}T{$horaFin}",
+        "backgroundColor" => $colorFondo,
+        "borderColor" => $colorFondo,
+        "textColor" => "#000000" ,
     ];
 }
 
 header('Content-Type: application/json');
-echo json_encode($clases, JSON_PRETTY_PRINT); 
+echo json_encode($clases, JSON_PRETTY_PRINT);
 exit();
-
 ?>
