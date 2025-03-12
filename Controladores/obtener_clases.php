e55687e9b94a0717ffe093d0681d4477b79501fd
PACJ26 <juan123alv@gmail.com>
2025-03-12 09:09:10 -0500

    1.se agregan variables para capturar datos del usuario

--- Controladores/obtener_clases.php
+++ Controladores/obtener_clases.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+include("../Conexion/conexion.php");
+
 function generarColorDesdeHora($hora) {
     $hash = md5($hora);
     $r = hexdec(substr($hash, 0, 2)) % 156 + 100; 
@@ -7,14 +10,27 @@ function generarColorDesdeHora($hora) {
     return "rgb($r, $g, $b)";
 }
 
-include("../Conexion/conexion.php");
+//datos del usuario autenticado
+$usuario_id = $_SESSION['usuario']['id']; 
+$rol = $_SESSION['usuario']['rol'];
 
 $sql = "SELECT c.id, a.nombre as asignatura, c.fecha, c.hora, c.hora_fin, c.estado, u.nombres as profesor 
         FROM clases c
         INNER JOIN asignaturas a ON c.asignatura_id = a.id 
         INNER JOIN usuarios u ON c.profesor_id = u.id";
 
+//segun rol mostrar clases
+if ($rol == 2) { // Profesor
+    $sql .= " WHERE c.profesor_id = $usuario_id";
+} elseif ($rol == 3) { // Estudiante
+    $sql .= " INNER JOIN inscripciones i ON c.id = i.clase_id WHERE i.estudiante_id = $usuario_id";
+}
+
 $resultado = $conexion->query($sql);
+
+if (!$resultado) {
+    die("Error en la consulta SQL: " . $conexion->error);
+}
 
 $clases = [];
 while ($fila = $resultado->fetch_assoc()) {
@@ -31,7 +47,7 @@ while ($fila = $resultado->fetch_assoc()) {
         "end" => "{$fecha}T{$horaFin}",
         "backgroundColor" => $colorFondo,
         "borderColor" => $colorFondo,
-        "textColor" => "#000000" ,
+        "textColor" => "#000000",
     ];
 }
 
